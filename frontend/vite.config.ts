82c8c25df60b0da74ec9a5a89c70e57dfd91aac1
KVBA-dev <kubaokon8@gmail.com>
2026-04-29 20:25:27 +0200

    chore(do): added hot reloading support for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,5 +8,12 @@ export default defineConfig({
   server: {
     port: 3000,
     host: '0.0.0.0'
-  }
+  },
+  watch: {
+    usePolling: true,
+    interval: 1000,
+  },
+  hmr: {
+    clientPort: 3000,
+  },
 })
