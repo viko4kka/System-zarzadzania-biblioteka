0b10b2efb495f6086ef7a29678ec287ccc72bb70
Viktoriia Kaspryk <147540172+viko4kka@users.noreply.github.com>
2026-05-05 11:37:29 +0200

    correct logout functionality (#74)

--- frontend/src/api/auth/auth.api.ts
+++ frontend/src/api/auth/auth.api.ts
@@ -1,32 +1,49 @@
 import { api } from "../api";
 import type {
-    LoginDto,
-    ApiLoginResponseDto,
-    User,
-    RegisterDto,
-    RemoveUserDto,
-    ApiRegisterResponseDto,
-    ApiRemoveUserResponseDto,
+  LoginDto,
+  ApiLoginResponseDto,
+  User,
+  RegisterDto,
+  RemoveUserDto,
+  ApiRegisterResponseDto,
+  ApiRemoveUserResponseDto,
 } from "./auth.types";
-import { mapLoginResponse,   mapRegisterResponse } from "./auth.mapper";
+import { mapLoginResponse, mapRegisterResponse } from "./auth.mapper";
 
 const baseURL = "/api/auth";
 
 export const authApi = {
-    login: async (data: LoginDto): Promise<{ user: User; message: string }> => {
-        const response = await api.post<ApiLoginResponseDto>(`${baseURL}/login`, data);
-        return mapLoginResponse(response);
-    },
+  login: async (data: LoginDto): Promise<{ user: User; message: string }> => {
+    const response = await api.post<ApiLoginResponseDto>(
+      `${baseURL}/login`,
+      data,
+    );
+    return mapLoginResponse(response);
+  },
 
-    register: async (data: RegisterDto): Promise<{ registeredUser: { id: string; name: string }; message: string }> => {
-        const response = await api.post<ApiRegisterResponseDto>(`${baseURL}/register`, data);
-        return mapRegisterResponse(response);
-    },
-    
-    //TODO: endpoint for logout
+  register: async (
+    data: RegisterDto,
+  ): Promise<{
+    registeredUser: { id: string; name: string };
+    message: string;
+  }> => {
+    const response = await api.post<ApiRegisterResponseDto>(
+      `${baseURL}/register`,
+      data,
+    );
+    return mapRegisterResponse(response);
+  },
 
-    removeUser: async (id: string, data: RemoveUserDto): Promise<string> => {
-        const response = await api.patch<ApiRemoveUserResponseDto>(`${baseURL}/removeUser/${id}`, data);
-        return response.message;
-    },
+  logout: async (): Promise<void> => {
+    const response = await api.post<void>(`${baseURL}/logout`);
+    return response;
+  },
+
+  removeUser: async (id: string, data: RemoveUserDto): Promise<string> => {
+    const response = await api.patch<ApiRemoveUserResponseDto>(
+      `${baseURL}/removeUser/${id}`,
+      data,
+    );
+    return response.message;
+  },
 };
