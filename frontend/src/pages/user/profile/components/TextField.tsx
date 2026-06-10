d14b2597cba56fd75430308a6cb3559c2be18e30
Michalooooooo <michal.niziolek04@gmail.com>
2026-06-10 16:54:07 +0200

    fix: Error handling, showing backend responses

---
+++ frontend/src/pages/user/profile/components/TextField.tsx
@@ -0,0 +1,40 @@
+import { type ChangeEvent } from "react";
+
+type TextFieldProps = {
+  placeholder?: string;
+  disabled?: boolean;
+  default_value?: string;
+  type?: "text" | "password"; 
+  value?: string;             
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void; 
+};
+
+export default function TextField({ 
+  placeholder, 
+  disabled = false, 
+  default_value,
+  type = "text", 
+  value,
+  onChange 
+}: TextFieldProps) {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      disabled={disabled}
+      defaultValue={default_value} 
+      value={value}                
+      onChange={onChange}          
+      className="
+        px-4 py-2
+        rounded-xl
+        border-2 border-gray-300
+        outline-none
+        focus:border-gray-500
+        transition
+        w-[50%]
+        mb-[1%]
+      "
+    />
+  );
+}
